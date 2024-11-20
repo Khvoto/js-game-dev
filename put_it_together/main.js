df45ddc58f3cb59f40c24820e20092b4a91617c1
Jens Österqvist <osterqvist@outlook.com>
2024-11-20 21:03:20 +0100

    all component parts done first game wip

---
+++ put_it_together/main.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
+ctx.font = '50px impact'
+let gameFrame = 0;
+let score = 0;
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0; 
+
+let ravens = [];
+
+class Raven {
+  constructor() {
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    this.sizeModifier = Math.random()
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
+    this.x = canvas.width;
+    this.y = Math.random() * (canvas.height - this.height);
+    this.directionX = Math.random() * 5 + 3;
+    this.directionY = Math.random() * 5 -  2.5;
+    this.markedForDeletion = false;
+
+    this.image = new Image();
+    this.image.src = './assets/raven.png';
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 50 + 50;
+
+    this.frame = 0; 
+    this.maxFrame = 4;
+  }
+
+  update(deltaTime) {
+    if (this.y < 0 || this.y > canvas.height - this.height) {
+      this.directionY = this.directionY * -1
+    }
+    this.x -= this.directionX;
+    this.y += this.directionY;
+    if ( this.x < 0 - this.width) this.markedForDeletion = true;
+    this.timeSinceFlap += deltaTime
+    if ( this.timeSinceFlap > this.flapInterval ) {
+      if ( this.frame > this.maxFrame) { this.frame = 0 }
+      else { this.frame++ }
+      this.timeSinceFlap = 0;
+    }
+  }
+  draw() {
+    ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+  }
+}
+
+const raven = new Raven
+
+function drawScore () {
+  ctx.fillStyle = 'black';
+  ctx.fillText('Score: ' + score, 48, 73)
+  ctx.fillStyle = 'white';
+  ctx.fillText('Score: ' + score, 50, 75);
+}
+
+window.addEventListener('click', (e) => {
+  const detectPixelData = ctx.getImageData( e.x, e.y, 1, 1 );
+  console.log(detectPixelData)
+})
+
+function animate(timestamp) {
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  timeToNextRaven += deltaTime
+  if(ravenInterval < timeToNextRaven) {
+    ravens.push(new Raven)
+    timeToNextRaven = 0;
+  }
+  drawScore();
+  [...ravens].forEach ( object => object.update(deltaTime));
+  [...ravens].forEach( object => object.draw());
+  ravens = ravens.filter(object => !object.markedForDeletion);
+
+  requestAnimationFrame(animate)
+}
+animate(0)
